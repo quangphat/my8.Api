11b93bf9a5f6bb54026d447bd8d41b1e9d289b44
quangphat <phat.dinhquang@haravan.com>
2018-04-10 17:28:19 +0700

    add

---
+++ my8.Api/Business/PersonBusiness.cs
@@ -0,0 +1,12 @@
+﻿using my8.Api.IBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my8.Api.Business
+{
+    public class PersonBusiness:IPersonBusiness
+    {
+    }
+}
---
+++ my8.Api/IBusiness/IPersonBusiness.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my8.Api.IBusiness
+{
+    interface IPersonBusiness
+    {
+        Task<bool> Create();
+    }
+}
