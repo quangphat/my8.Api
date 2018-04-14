faab485cbb30286e69d3f592e6c3129007537de3
quangphat <phat.dinhquang@haravan.com>
2018-04-14 13:36:44 +0700

    f

---
+++ my8.Api/Models/ClubAllin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my8.Api.Models
+{
+    public class ClubAllin
+    {
+        public Club Club {get;set;}
+        public int Joins { get; set; }
+        public int Total { get; set; }
+    }
+}
